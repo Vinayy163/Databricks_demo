3129b2a8090b659c27ce4688359ba4d75424de07
U6080324 <abhin@gmail.com>
2026-09-10 10:28:59 +0530

    added substraction

--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,10 @@
-from calculator import add,multiply
+from calculator import add,multiply,substract
 
 def test_add():
   assert add(2,3)==5
 
 def test_multiply():
   assert multiply(3,4)==12
+
+def test_substract():
+  assert substract(5,2)==3
